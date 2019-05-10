a625439378b05e067cea8b0ac5c2bcce62af4f34
Angela Madjirova <angelamadjirova@gmail.com>
2019-05-10 07:48:56 +0200

    Removing unnecessary info

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,18 +41,18 @@
             @endforeach
         </div>
     @endif
-<div class="flex-center full-height">
-    <div>
-        <label for="search-box"><img alt="logo" src="/img/logo.svg" class="img" /></label>
-    </div>
-    <div class="search">
-        <form method="post" action="/search">
-            {{csrf_field()}}
-            <input id="search-box" name="query" type="text" placeholder="search term" />
-            <button type="submit">Search for Artists</button>
+    <div class="flex-center full-height">
+        <div>
+            <label for="search-box"><img alt="logo" src="/img/logo.svg" class="img" /></label>
+        </div>
+        <div class="search">
+            <form method="post" action="/search">
+                {{csrf_field()}}
+                <input id="search-box" name="query" type="text" placeholder="search term" />
+                <button type="submit">Search for Artists</button>
 
-        </form>
+            </form>
+        </div>
     </div>
-</div>
 </body>
 </html>
